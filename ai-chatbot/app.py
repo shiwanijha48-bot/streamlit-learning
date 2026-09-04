a6a080d912277e17b264d19910d379ca959bbd81
Shiwani Jha <shiwanijha48@gmail.com>
2026-09-05 01:15:39 +0530

    Fix API key retrieval and improve chat input handling- stage 3

--- ai-chatbot/app.py
+++ ai-chatbot/app.py
@@ -3,24 +3,52 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
+# Load API key
 load_dotenv()
-api_key = os.getenv("GEMINI_API_KEYT")
+api_key = os.getenv("GEMINI_API_KEY")
+# Gemini client
 client = genai.Client(api_key=api_key)
 
+# -------------------------
+# Session State
+# -------------------------
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
+# -------------------------
+# UI
+# -------------------------
 st.title("AI Chatbot")
 st.write("Ask me anything!")
 
-question = st.text_input("Enter your Question")
-if st.button("Ask AI"):
-    if question:
+# show previous messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+#  user input
+question = st.chat_input("Enter your Question")
+# when user sends message
+if question:
+    # show user message
+    with st.chat_message("user"):
+        st.markdown(question)
+    # save user message
+    st.session_state.messages.append({
+        "role" :"user",
+        "content":question
+    })
+    # get AI respoense
+    with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             response = client.models.generate_content(
-                model = "gemini-3.6-flash",
-                contents=question
-            )
-        st.write("You asked:", question)
-        st.write(response.text)
+                model="gemini-3.6-flash",
+                contents=question)
+            answer= response.text
+        st.markdown(answer)
 
-    else:
-        st.warning("Please enter your question.")
+    # save AI response
+    st.session_state.messages.append({
+        "role":"assistant",
+        "content":answer
+    })
