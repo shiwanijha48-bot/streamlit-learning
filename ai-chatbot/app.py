b5cb5525cd76ff10535e6cafe553b48f678b7164
Shiwani Jha <shiwanijha48@gmail.com>
2026-09-04 23:55:35 +0530

    basic layout

--- ai-chatbot/app.py
+++ ai-chatbot/app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEYT")
+client = genai.Client(api_key=api_key)
+
 
 st.title("AI Chatbot")
 st.write("Ask me anything!")
@@ -6,6 +14,13 @@ st.write("Ask me anything!")
 question = st.text_input("Enter your Question")
 if st.button("Ask AI"):
     if question:
+        with st.spinner("Thinking..."):
+            response = client.models.generate_content(
+                model = "gemini-3.6-flash",
+                contents=question
+            )
         st.write("You asked:", question)
+        st.write(response.text)
+
     else:
         st.warning("Please enter your question.")
